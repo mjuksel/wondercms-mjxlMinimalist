ea805a7e37b8f5763058343d207fcab62fb77514
Mjuksel <mportma@gmail.com>
2021-03-22 23:29:58 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+Minimalist Theme for _[WonderCMS]_
+====
+
+![](preview.jpg)
+
+[WonderCMS]:https://wondercms.com
---
+++ theme.php
@@ -0,0 +1,23 @@
+<?php global $Wcms; ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title><?= $Wcms->get('config', 'siteTitle') ?></title>
+	<meta name="description" content="<?= $Wcms->page('description') ?>">
+	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<?php if($Wcms->loggedIn) { ?> <?php } ?>
+	<link rel="stylesheet" href="<?= $Wcms->asset('css/user-style.css') ?>">
+	<?php if ($Wcms->loggedIn) { ?>
+		<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+	<?php } ?>
+	</head>
+	<body>
+		<?= $Wcms->alerts() ?>
+		<?= $Wcms->settings() ?>
+		<nav><ul><?= $Wcms->menu() ?></ul></nav>
+		<section><?= $Wcms->page('content') ?></section>
+		<footer><?= $Wcms->footer() ?></footer>
+		<?= $Wcms->js() ?>
+</body>
+</html>
